87561328669c211efc2980c7b4a69183c222b825
Arkadiusz Cieslak <arekc70@gmail.com>
2019-06-28 12:55:04 +0200

    Removed secretkey

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAtwaI7uXNN7ZuLLtJbZJsnzmV5UYr3kzw';
+const KEY = 'SecretKey';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
